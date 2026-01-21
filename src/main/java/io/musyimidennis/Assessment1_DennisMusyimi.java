af93173b09660a2bfc96a89db246a3aa6972eed1
Musyimi jr <denniskitaka5@gmail.com>
2026-01-21 16:52:36 +0300

    Improved the clarity on the sections of Questions

--- src/main/java/io/musyimidennis/Assessment1_DennisMusyimi.java
+++ src/main/java/io/musyimidennis/Assessment1_DennisMusyimi.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class Assessment1_DennisMusyimi {
     public static void main(String[] args) {
         // --QUIZ PROBLEM 1 VARIABLE OPERATIONS--//
+        System.out.println("---VARIABLE OPERATIONS---");
         // Declare integers//
         int a = 15;
         int b = 4;
@@ -23,7 +24,7 @@ public class Assessment1_DennisMusyimi {
         System.out.println("The remainder of a divided by b is: " + remainder);
 
         //---QUIZ PROBLEM 2  GRADE CALCULATOR---//
-
+        System.out.println("---GRADE CALCULATOR---");
         System.out.print("Enter the Student's score (0-100): ");
         int score = new Scanner(System.in).nextInt();
         if (score < 0 || score > 100) {
@@ -41,6 +42,7 @@ public class Assessment1_DennisMusyimi {
         }
 
         //---QUIZ PROBLEM 3  NUMBER SEQUENCE---//
+        System.out.println("---NUMBER SEQUENCE---");
         for (int i = 1; i <= 20; i++) {
             System.out.println(i);
             if (i % 5 == 0) {
@@ -55,6 +57,7 @@ public class Assessment1_DennisMusyimi {
         }
 
        // PROBLEM 4: INPUT VALIDATION
+        System.out.println("---INPUT VALIDATION---");
         Scanner scanner = new Scanner(System.in);
         int number;
 
@@ -88,6 +91,7 @@ public class Assessment1_DennisMusyimi {
 
 
         //PROBLEM 5 PATTERN PRINTING//
+        System.out.println("---PATTERN PRINTING---");
 
         int rows = 5;
 
