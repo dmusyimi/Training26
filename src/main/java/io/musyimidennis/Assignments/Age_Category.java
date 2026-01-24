17a68c19a08db0852a66f30c46ed2afa5b010843
Musyimi jr <denniskitaka5@gmail.com>
2026-01-25 02:16:58 +0300

    more practice

---
+++ src/main/java/io/musyimidennis/Assignments/Age_Category.java
@@ -0,0 +1,28 @@
+package io.musyimidennis.Assignments;
+
+import java.util.Scanner;
+
+public class Age_Category {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age: ");
+        int age = scanner.nextInt();
+        if (age >= 0 && age <= 2) {
+            System.out.println("infant!");
+        }else if (age>=3 && age<=12) {
+            System.out.println("child!");
+          } else if (age>=13 && age<=19) {
+             System.out.println("Teenager!");
+            } else if (age>=20 && age<=64) {
+            System.out.println("Adult");
+              } else if (age>=65){
+            System.out.println("Senior ");
+
+
+
+        }
+
+    }
+
+
+    }
---
+++ src/main/java/io/musyimidennis/Assignments/Multiplication_table.java
@@ -0,0 +1,14 @@
+package io.musyimidennis.Assignments;
+
+import java.util.Scanner;
+
+public class Multiplication_table {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number = scanner.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
+    }
+}
