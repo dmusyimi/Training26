bb580a80de3fd1f7576d49b18f9946561b0d8db1
Musyimi jr <denniskitaka5@gmail.com>
2026-01-25 00:44:03 +0300

    BMI Calculator

---
+++ src/main/java/io/musyimidennis/Assignments/BMI_Calculator.java
@@ -0,0 +1,15 @@
+package io.musyimidennis.Assignments;
+
+import java.util.Scanner;
+
+public class BMI_Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your weight in kilograms: ");
+        double weight = scanner.nextDouble();
+        System.out.println("Enter your height in meters: ");
+        double height = scanner.nextDouble();
+        double bmi = weight / (height * height);
+        System.out.println("Your BMI is: " + bmi);
+    }
+}
